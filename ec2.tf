7d7295252eb6bef294d2800cef26c37a66e245aa
Gupta <sneha.c.gupta@capgemini.com>
2024-05-16 14:39:52 +0530

    new commit

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-/*resource "aws_instance" "newec2" {
+resource "aws_instance" "newec2" {
   ami = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
 }
 resource "aws_ec2_instance_state" "test" {
   instance_id = aws_instance.newec2.id
   state       = "running"
-}*/
+}
