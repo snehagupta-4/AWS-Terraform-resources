7de87b3dcb35d51884d90b5fdf6b5ace9f4192e3
Gupta <sneha.c.gupta@capgemini.com>
2024-05-20 11:25:40 +0530

    commit

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -4,5 +4,10 @@ resource "aws_glue_job" "jsontocsv" {
 
   command {
     script_location = "s3://${aws_s3_bucket.bucket.bucket}/awsgluescript.py"
+    python_version = 3
+  }
+
+  provisioner "local-exec" {
+    command = "aws glue start-job-run --job-name ${aws_glue_job.jsontocsv.name} --region ${var.region}"
   }
 }
